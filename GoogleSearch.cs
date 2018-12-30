aaae0535cc021ac11f99dbb132300f0122d4e744
Paweł Pałasz <ppalasz@poczta.onet.pl>
2018-12-30 14:40:53 +0100

    Dodaj pliki projektów.

---
+++ GoogleSearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Extensions;
+
+namespace SeleniumTests1
+{
+    [TestClass]
+    public class GNSS_definicja : TestGeo
+    {
+        
+
+        //[ExpectedException(typeof(ArgumentException))]
+        //[Timeout(TestTimeout.Infinite)]  // Milliseconds
+        [TestMethod]
+        public void PagingArticle()
+        {
+            testName = "PageParts";
+
+            using (var driver = new ChromeDriver(options))
+            {
+                driver.Manage().Window.Maximize();
+                string url1 = $"{www}/GNSS/definicja";
+                driver.Navigate().GoToUrl(url1);
+                string url2 = driver.Url;
+                Assert.AreEqual(url1, url2, $"1) link '{url1}' must be navigated to");
+
+                var submenu = driver.FindElementById("submenu");
+                Assert.IsNotNull(submenu, @"submenu must exist");
+
+                var chosenMenu = driver.FindElementByClassName("chosen_menu");
+                Assert.IsNotNull(chosenMenu, @"GNSS/definicja must be chosen");
+                chosenMenu.Click();
+                url2=driver.Url;
+                Assert.AreEqual(url1,url2, $"2) link '{url1}' must be navigated to");
+                //searchBox.SendKeys("test");
+                //var searchButton = driver.FindElementById("search_button");
+                //searchButton.Submit();
+                //var searchResults = driver.FindElementById("search_button");
+                SeleniumTestHelpLibrary.TakeScreenshot(driver, subfolder, testName);
+            }
+        }
+    }
+
+    public class TestGeo
+    {
+        protected string www = @"https://geoforum.pl";
+        protected string testName = "";
+        protected string subfolder = DateTime.Now.ToShortDateString().Replace(":", "_");
+        protected ChromeOptions options = SeleniumTestHelpLibrary.GetChromeOptions();
+    }
+}
---
+++ SeleniumTestHelpLibrary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests1
+{
+    public static class SeleniumTestHelpLibrary
+    {
+        public static void TakeScreenshot(object driver,string subfolder, string title)
+        {
+            title = title.Replace(" ", "_");
+            string folder = $"{System.IO.Path.GetFullPath(@"..\..\")}\\screenshots\\{subfolder}";
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            string file = $"TestScreenShot_{DateTime.Now.ToShortDateString().Replace(":","_")}_{DateTime.Now.ToShortTimeString().Replace(":", "_")}.jpg";
+            try
+            {
+                Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+                ss.SaveAsFile($"{folder}\\{file}", ScreenshotImageFormat.Jpeg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public static ChromeOptions GetChromeOptions()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("window-size=1920,1080"); // open Browser in maximized mode
+            //options.AddArgument("disable-infobars"); // disabling infobars
+            //options.AddArgument("--disable-extensions"); // disabling extensions
+            //options.AddArgument("--disable-gpu"); // applicable to windows os only
+            //options.AddArgument("--disable-dev-shm-usage"); // overcome limited resource problems
+            //options.AddArgument("--no-sandbox"); // Bypass OS security model
+
+            return (options);
+        }
+    }
+}
